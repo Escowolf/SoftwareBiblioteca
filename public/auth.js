89570c7e4a7606a89eee204f6d83c5d89a90deaa
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-11-17 20:46:58 -0300

    node

--- public/auth.js
+++ public/auth.js
@@ -1,1 +1,13 @@
-console.log('Hello node');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+    res.send('Hello')
+})
+
+app.listen(4000);

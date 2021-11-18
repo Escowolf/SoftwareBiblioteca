335c895d870b48f41a479422f2c1c7464e5e165c
Ezequiel Sousa Silva <63311919+Escowolf@users.noreply.github.com>
2021-11-17 21:40:01 -0300

    first auth firebase

--- public/auth.js
+++ public/auth.js
@@ -1,13 +1,55 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
 
-const app = express();
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-app.use(bodyParser.urlencoded({extended:true}));
+ui.start('#firebaseui-auth-container', {//Endereço de e-mail e senha
+    signInOptions: [
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
 
-app.get('/',(req,res)=>{
-    res.send('Hello')
-})
+ui.start('#firebaseui-auth-container', {//Autenticação de link de e-mail
+    signInOptions: [
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+      }
+    ],
+    // Other config options...
+  });
 
-app.listen(4000);
+// Is there an email link sign-in?
+if (ui.isPendingRedirect()) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+  // This can also be done via:
+  if (firebase.auth().isSignInWithEmailLink(window.location.href)) {
+    ui.start('#firebaseui-auth-container', uiConfig);
+  }
+
+  ui.start('#firebaseui-auth-container', {//Provedores de OAuth (Google, Facebook, Twitter e GitHub)
+    signInOptions: [
+      // List of OAuth providers supported.
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
+  });
+/*import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+const auth = getAuth();
+signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });*/
